5eafea10fa2075b19ef830fca0f007037d60f960
chathurathamakariyo <chathurahansaka229@gmail.com>
2026-06-28 23:38:20 +0530

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
     async function findWorkingDomain(quality) {
       for (let i = 1; i <= 45; i++) {
         const num = String(i).padStart(2, "0");
-        const domain = `https://bot${num}.shenaya66.online/`;
+        const domain = `https://${num}.shenaya66.online/`;
         const testUrl = domain + cleanName.replace(".mp4", `${quality}.mp4`);
 
         try {
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
         }
       }
       // fallback to 01 if nothing found
-      return `https://01.yadev511.xyz/${cleanName.replace(".mp4", `${quality}.mp4`)}`;
+      return `https://01.shenaya66.online/${cleanName.replace(".mp4", `${quality}.mp4`)}`;
     }
 
     // Run all quality checks in parallel
